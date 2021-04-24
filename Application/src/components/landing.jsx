e12ed48d19258e0fffe33938456a2d81f41f20bc
Zhenlong123 <30749747+Zhenlong12345@users.noreply.github.com>
2021-04-24 16:57:39 -0700

    Update landing.jsx

--- Application/src/components/landing.jsx
+++ Application/src/components/landing.jsx
@@ -11,31 +11,31 @@ class LandingPage extends Component {
         const isUserStaff = UserServiceApi.isUserStaff();
         return (
                 <Container id="landing-page" fluid>
-                    <div >
-                        <h1 >Uber Bus</h1>
+//                     <div >
+//                         <h1 >Uber Bus</h1>
 
-                        {(isUserLoggedIn && !isUserStaff) &&
-                            <>
-                                <Link to="/dashboard">
-                                    <Button variant="warning" style={{ fontSize: '2vh' }}>Dashboard</Button>
-                                </Link>
-                            </>
-                        }
-                        {(isUserLoggedIn && isUserStaff) &&
-                            <>
-                                <Link to="/staff">
-                                    <Button variant="warning" style={{ fontSize: '2vh' }}>Manage System</Button>
-                                </Link>
-                            </>
-                        }
-                        {!isUserLoggedIn &&
-                            <>
-                                <Link to="/signup">
-                                    <Button variant="warning" style={{ fontSize: '2vh' }}>Sign Up Now</Button>
-                                </Link>
-                            </>
-                        }
-                    </div>
+//                         {(isUserLoggedIn && !isUserStaff) &&
+//                             <>
+//                                 <Link to="/dashboard">
+//                                     <Button variant="warning" style={{ fontSize: '2vh' }}>Dashboard</Button>
+//                                 </Link>
+//                             </>
+//                         }
+//                         {(isUserLoggedIn && isUserStaff) &&
+//                             <>
+//                                 <Link to="/staff">
+//                                     <Button variant="warning" style={{ fontSize: '2vh' }}>Manage System</Button>
+//                                 </Link>
+//                             </>
+//                         }
+//                         {!isUserLoggedIn &&
+//                             <>
+//                                 <Link to="/signup">
+//                                     <Button variant="warning" style={{ fontSize: '2vh' }}>Sign Up Now</Button>
+//                                 </Link>
+//                             </>
+//                         }
+//                     </div>
                 </Container>
         )
     }
